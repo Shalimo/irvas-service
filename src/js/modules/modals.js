59b3e1a0b6523b4a801fd2ee399a01dbe64464e6
Nikita Shalimo <MegaFrostBall@gmail.com>
2021-10-04 16:46:58 +0300

    modernize showing of the modal window with static display body using the width of the scroll

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -4,10 +4,27 @@ const modals = (modalSelector, openButton, closeButton, closeByOverlay = true) =
     const closeModalBtn = document.querySelectorAll(closeButton);
     const modals = document.querySelectorAll('[data-modal]');
     const submitButtons = document.querySelectorAll('[name="submit"]');
+    scroll = calcScroll();
+
+    function calcScroll() {
+        const div = document.createElement('div');
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+
+        const scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+    }
 
     function hideAllModals() {
         modals.forEach(item => {
             item.style.display = 'none';
+            document.body.style.marginRight = `0px`
         });
     }
 
@@ -15,12 +32,14 @@ const modals = (modalSelector, openButton, closeButton, closeByOverlay = true) =
         modal.classList.add('show');
         modal.classList.remove('hide');
         document.body.style.overflow = 'hidden';
+        document.body.style.marginRight = `${scroll}px`
     }
 
     function hideModals() {
         modal.classList.add('hide');
         modal.classList.remove('show');
         document.body.style.overflow = '';
+        document.body.style.marginRight = `0px`
     }
 
     openModalBtn.forEach(item => {
@@ -65,6 +84,7 @@ const modals = (modalSelector, openButton, closeButton, closeByOverlay = true) =
                 modal.classList.add('show');
                 modal.classList.remove('hide');
                 document.body.style.overflow = 'hidden';
+                document.body.style.marginRight = `${scroll}px`
         }, time)
     }
 
